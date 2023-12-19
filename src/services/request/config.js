46d90dd43d24a0ed362b3137e69c0e51c1438803
niqifan007 <83665513+niqifan007@users.noreply.github.com>
2023-12-19 09:11:14 +0000

    Update BASE_URL in config.js

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "http://123.207.32.32:1888/api"
-export const BASE_URL = "https://vercel-api-proxy-rust.vercel.app/http://codercba.com:1888/api"
+//export const BASE_URL = "http://123.207.32.32:1888/api"
+export const BASE_URL = "https://api.wakaplus.com/api"
 export const TIMEOUT = 10000
