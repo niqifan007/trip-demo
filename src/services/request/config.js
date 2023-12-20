f8b9d4d71edc0cbc805b8f94d3a6a9f908e4561f
niqifan007 <83665513+niqifan007@users.noreply.github.com>
2023-12-20 02:54:08 +0000

    Update page title and API base URL

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,3 +1,4 @@
 //export const BASE_URL = "http://123.207.32.32:1888/api"
-export const BASE_URL = "https://api.wakaplus.com/api"
+//export const BASE_URL = "https://api.wakaplus.com/api"
+export const BASE_URL = "/api"
 export const TIMEOUT = 10000
